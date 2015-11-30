2f34fe480a4ac646ad963aa1691dd1acf34eedcd
Zhiqiang Ye <xiqian013@live.com>
2015-11-30 15:52:40 +0800

    github.com/zhaochenyougi

---
+++ codes/001_Foundations/004_Divide_and_Conquer/Strassen/general_head.h
@@ -0,0 +1,26 @@
+//通用头文件
+//general_head.h
+
+//支持所有算法的通用头文件
+
+#include <iostream>
+#include <cstring>
+#include <utility>
+#include <cmath>
+#include <algorithm>
+
+#include <string>
+#include <sstream>
+#include <vector>
+#include <deque>
+#include <map>
+#include <set>
+#include <stack>
+#include <queue>
+#if defined(WIN32) || defined(_WIN32)
+#include <windows.h>
+#endif
+using namespace std;
+
+#define MAX 60			//最大值
+#define INF 99999		//极大值
